440d01075786532a4ce81b47093dfbdd8121d8f4
Paul Okello <paulokello273@gmail.com>
2021-01-10 14:30:25 +0300

    🌌🌌Webcam and canvas setup🌌🌌

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
-import React from "react";
+import React, { useRef } from "react";
 import * as tf from "@tensorflow/tfjs";
 import * as handpose from "@tensorflow-models/handpose";
 import Webcam from "react-webcam";
 
 const App = () => {
-  return <div></div>;
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+  return (
+    <div>
+      <Webcam
+        ref={webcamRef}
+        style={{
+          position: "absolute",
+          marginLeft: "auto",
+          marginRight: "auto",
+          left: 0,
+          right: 0,
+          textAlign: "center",
+          zIndex: 9,
+          width: 640,
+          height: 480,
+        }}
+      />
+      <canvas
+        ref={canvasRef}
+        style={{
+          position: "absolute",
+          marginLeft: "auto",
+          marginRight: "auto",
+          left: 0,
+          right: 0,
+          textAlign: "center",
+          zIndex: 9,
+          width: 640,
+          height: 480,
+        }}
+      />
+    </div>
+  );
 };
 
 export default App;
